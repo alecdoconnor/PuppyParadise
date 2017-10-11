2e3259f4629ef36dfbf864e2df528b885a037d07
Alec O’Connor <Alec@AlecOConnor.com>
2017-10-10 23:16:56 -0500

    Initial Commit

---
+++ PuppyParadise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PuppyParadise
+//
+//  Created by Alec O'Connor on 10/10/17.
+//  Copyright © 2017 Alec O'Connor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
